498268e6e8d62fd84b52cc9fff5a36d7153e8d4f
Daniel Miranda <mastermsn@gmail.com>
2023-03-07 18:58:22 -0300

    corrección jwt

--- src/config/env.js
+++ src/config/env.js
@@ -1,13 +1,12 @@
 require('dotenv').config({ override: true });
 
 module.exports = {
-    PORT: parseInt(process.env.PORT),
-    HOST: process.env.HOST,
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    DB: process.env.DB,
-    SALT: parseInt(process.env.SALT),
-    PASS: process.env.PASS,
-    TOKEN_SECRET: process.env.TOKEN_SECRET,
-    TOKEN_AUTH_NAME: process.env.TOKEN_AUTH_NAME
-  }
+  PORT: parseInt(process.env.PORT),
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
+  SALT: parseInt(process.env.SALT),
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  TOKEN_AUTH_NAME: process.env.TOKEN_AUTH_NAME
+}
--- src/middleware/authToken.js
+++ src/middleware/authToken.js
@@ -3,43 +3,44 @@ const jwt = require('jsonwebtoken');
 const moment = require('moment');
 
 exports.validarToken = (req, res, next) => {
-    try {
-        const token = req.header(config.TOKEN_AUTH_NAME);
-        //validar si llegó el token
-        if (!token)
-            return res
-                .status(401)
-                .send({
-                    status: "FAILED",
-                    data: { error: "Acceso denegado" },
-                });
+  try {
+    const token = req.header(config.TOKEN_AUTH_NAME);
+    //validar si llegó el token
+    if (!token)
+      return res
+        .status(401)
+        .send({
+          status: "FAILED",
+          data: { error: "Acceso denegado" },
+        });
 
-        const tokenDecode = jwt.decode(token, config.TOKEN_SECRET);
-        //validar token
-        if (!tokenDecode)
-            return res
-                .status(401)
-                .send({
-                    status: "FAILED",
-                    data: { error: "Acceso denegado" },
-                });
+    const tokenDecode = jwt.decode(token, config.TOKEN_SECRET);
+    //validar token
+    const validarToken = jwt.verify(token, config.TOKEN_SECRET);
+    if (!tokenDecode)
+      return res
+        .status(401)
+        .send({
+          status: "FAILED",
+          data: { error: "Acceso denegado" },
+        });
 
-        // comprobar si el token ha expirado
-        if (tokenDecode.tiempoExpiracion <= moment().unix()) {
-            return res
-                .status(401)
-                .send({
-                    status: "FAILED",
-                    data: { error: "Acceso denegado, Token Expirado" },
-                });
-        }
-
-        next() // continuamos
-
-    } catch (error) {
-        res
-            .status(error?.status || 500)
-            .send({ status: "FAILED", data: { error: error?.message || error } });
+    // comprobar si el token ha expirado
+    if (tokenDecode.tiempoExpiracion <= moment().unix()) {
+      return res
+        .status(401)
+        .send({
+          status: "FAILED",
+          data: { error: "Acceso denegado, Token Expirado" },
+        });
     }
+
+    next() // continuamos
+
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 
 }
